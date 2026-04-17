ae0123ce9240e6b4772cc27797dfb1f479a9a44f
Mayank Bisht <mayankmb2006@gmail.com>
2026-04-17 15:28:29 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/health', (req, res) => {
+  res.json({ status: 'OK', message: 'Server is running' });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+module.exports = app;
